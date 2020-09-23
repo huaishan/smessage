c3afdad0e3694c721f78a937c5bdcda6521627d8
feng <fabledcool@gmail.com>
2020-09-24 00:56:29 +0800

    test

--- message/message_test.go
+++ message/message_test.go
@@ -5,27 +5,36 @@ import (
 )
 
 func TestNewCommonMessageShouldReturnsAMessage(t *testing.T) {
-	NewCommonMessage("body")
+	NewCommonMessage("body", "test", "msg")
 }
 
 func TestCommonMessageGetContentShouldReturnsContent(t *testing.T) {
-	m := NewCommonMessage("content")
+	m := NewCommonMessage("body", "test", "content")
+
+	if m.GetSender() != "body" {
+		t.Error("should returns 'body', got: ", m.GetSender())
+	}
+
+	if m.GetChannel() != "test" {
+		t.Error("should returns 'test', got: ", m.GetChannel())
+	}
 
 	if m.GetContent() != "content" {
-		t.Error("should returns content, got: ", m.GetContent())
+		t.Error("should returns 'content', got: ", m.GetContent())
 	}
+
 }
 
 func TestCommonMessageDumpShouldReturnsDumpedString(t *testing.T) {
-	m := NewCommonMessage("content")
+	m := NewCommonMessage("body", "test", "content")
 
-	if m.Dump() != `{"msg":"content"}` {
-		t.Error(`should returns '{"msg":"content"}', got: `, m.Dump())
+	if m.Dump() != `{"sender":"body","channel":"test","msg":"content"}` {
+		t.Error(`should returns '{"sender":"body","channel":"test","msg":"content"}', got: `, m.Dump())
 	}
 }
 
 func TestLoadShouldReturnErrInvalidMessageWhenUnmarshalError(t *testing.T) {
-	_, err := Load("")
+	_, err := Load("body", "test", "")
 
 	if err != ErrInvalidMessage {
 		t.Error("err should be ErrInvalidMessage, got: ", err)
@@ -33,7 +42,7 @@ func TestLoadShouldReturnErrInvalidMessageWhenUnmarshalError(t *testing.T) {
 }
 
 func TestLoadShouldReturnErrInvalidMessageNoContentWhenNoMessageContent(t *testing.T) {
-	_, err := Load(`{}`)
+	_, err := Load("body", "test",`{}`)
 
 	if err != ErrInvalidMessageNoContent {
 		t.Error("err should be ErrInvalidMessageNoContent, got: ", err)
@@ -41,7 +50,7 @@ func TestLoadShouldReturnErrInvalidMessageNoContentWhenNoMessageContent(t *testi
 }
 
 func TestLoadShouldReturnErrInvalidMessageContentWhenMessageContentInvalid(t *testing.T) {
-	_, err := Load(`{"msg":1}`)
+	_, err := Load("body", "test",`{"msg":1}`)
 
 	if err != ErrInvalidMessageContent {
 		t.Error("err should be ErrInvalidMessageContent, got: ", err)
@@ -49,7 +58,7 @@ func TestLoadShouldReturnErrInvalidMessageContentWhenMessageContentInvalid(t *te
 }
 
 func TestLoadShouldReturnNoErrorWhenSucceed(t *testing.T) {
-	_, err := Load(`{"msg":""}`)
+	_, err := Load("body", "test",`{"msg":""}`)
 
 	if err != nil {
 		t.Error("err should be nil, got: ", err)
